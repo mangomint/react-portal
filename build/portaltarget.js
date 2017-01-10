d59f35b7d0ee811c1321436326cbbdf4f69bf2b9
hypnos <Filipp.Riabchun@jetbrains.com>
2017-01-10 17:02:41 +0300

    build

--- build/portaltarget.js
+++ build/portaltarget.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-exports.default = PortalTarget;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
@@ -16,19 +16,45 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-function PortalTarget(props) {
-  var name = props.name,
-      children = props.children,
-      restProps = _objectWithoutProperties(props, ['name', 'children']);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  return _react2.default.createElement(
-    'div',
-    _extends({
-      'data-portaltarget': name
-    }, restProps),
-    children
-  );
-}
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* eslint-disable react/prefer-stateless-function */
+
+
+var PortalTarget = function (_Component) {
+  _inherits(PortalTarget, _Component);
+
+  function PortalTarget() {
+    _classCallCheck(this, PortalTarget);
+
+    return _possibleConstructorReturn(this, (PortalTarget.__proto__ || Object.getPrototypeOf(PortalTarget)).apply(this, arguments));
+  }
+
+  _createClass(PortalTarget, [{
+    key: 'render',
+    value: function render() {
+      var _props = this.props,
+          name = _props.name,
+          children = _props.children,
+          restProps = _objectWithoutProperties(_props, ['name', 'children']);
+
+      return _react2.default.createElement(
+        'div',
+        _extends({
+          'data-portaltarget': name
+        }, restProps),
+        children
+      );
+    }
+  }]);
+
+  return PortalTarget;
+}(_react.Component);
+
+exports.default = PortalTarget;
+
 
 PortalTarget.propTypes = {
   name: _react.PropTypes.string,
