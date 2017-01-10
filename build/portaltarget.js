a4721bfc74ac9d4c2bae7d4ac4ef1ba8cd2ca722
hypnos <Filipp.Riabchun@jetbrains.com>
2017-01-10 16:45:56 +0300

    build

--- build/portaltarget.js
+++ build/portaltarget.js
@@ -3,6 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 exports.default = PortalTarget;
 
 var _react = require('react');
@@ -11,21 +14,23 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 function PortalTarget(props) {
+  var name = props.name,
+      children = props.children,
+      restProps = _objectWithoutProperties(props, ['name', 'children']);
+
   return _react2.default.createElement(
     'div',
-    {
-      className: props.className,
-      style: props.style,
-      'data-portaltarget': props.name
-    },
-    props.children
+    _extends({
+      'data-portaltarget': name
+    }, restProps),
+    children
   );
 }
 
 PortalTarget.propTypes = {
-  className: _react.PropTypes.string,
-  style: _react.PropTypes.object,
   name: _react.PropTypes.string,
   children: _react.PropTypes.node
 };
